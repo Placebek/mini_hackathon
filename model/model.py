d5477c54b6716ab27cd32de22c6d5a6cd8b6ed54
sakenzi <ermekulysaken8@gmail.com>
2024-12-23 15:25:50 +0600

    Добавлено модель Burger

--- model/model.py
+++ model/model.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Date, Integer, String, ForeignKey, TIMESTAMP
+from sqlalchemy import Column, Date, Integer, String, ForeignKey, TIMESTAMP, Float
 from sqlalchemy.orm import relationship
 
 from database.db import Base 
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -17,20 +17,21 @@ class Users(Base):
     main_card = relationship("MainCard", back_populates="user", cascade="all, delete")
 
 
-
 class BonusCard(Base):
-    __tablename__ = "bouns_cards"
+    __tablename__ = "bonus_cards"
 
     id = Column(Integer, primary_key=True, index=True)
     card_number = Column(String, nullable=False)
     money = Column(Integer)
-    percent = Column(Integer)
     replenishment_time = Column(Date)
     created_at = Column(TIMESTAMP, default=datetime.now)
 
 
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
-    user = relationship("User", back_populates="bouns_cards")
+    user = relationship("User", back_populates="bonus_cards")
+    
+    burger_id = Column(Integer, ForeignKey("burger.id", ondelete="CASCADE"))
+    burger = relationship("Burger", back_populates="burgers")
 
 class MainCard(Base):
     __tablename__ = "main_cards"
@@ -46,4 +47,14 @@ class MainCard(Base):
     user = relationship("User", back_populates="main_cards")
 
     
-    
+class Burger(Base):
+    __tablename__ = "burgers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name_burger = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    created_at = Column(TIMESTAMP, default=datetime.now)
+    precent = Column(Float, nullable=False)
+
+    bonus_id = Column(Integer, ForeignKey("bonus.id", ondelete="CASCADE"))
+    bonus = relationship("BonusCard", back_populates="bonus_cards")
