6c2ad820b499b6d7cfd46cab55cfc53f6b5c2a98
dikoshs <dianaserik307@gmail.com>
2024-12-23 13:06:53 +0300

    Second database model schema

--- model/model.py
+++ model/model.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Date, Integer, String, ForeignKey
+from datetime import datetime
+
+from sqlalchemy import Column, Date, Integer, String, ForeignKey, TIMESTAMP
 from sqlalchemy.orm import relationship
 
 from database.db import Base 
@@ -9,6 +11,7 @@ class Users(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, default=datetime.now)
 
     bonus_card = relationship("BonusCard", back_populates="user", cascade="all, delete")
     main_card = relationship("MainCard", back_populates="user", cascade="all, delete")
@@ -23,6 +26,8 @@ class BonusCard(Base):
     money = Column(Integer)
     percent = Column(Integer)
     replenishment_time = Column(Date)
+    created_at = Column(TIMESTAMP, default=datetime.now)
+
 
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
     user = relationship("User", back_populates="bouns_cards")
@@ -33,8 +38,9 @@ class MainCard(Base):
     id = Column(Integer, primary_key=True, index=True)
     card_number = Column(String, nullable=False)
     money = Column(Integer)
-    percent = Column(Integer)
     replenishment_time = Column(Date)
+    replenishment_money = Column(Integer)
+    created_at = Column(TIMESTAMP, default=datetime.now)
 
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
     user = relationship("User", back_populates="main_cards")
